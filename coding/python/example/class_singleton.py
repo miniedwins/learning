a620177d9a9758d9ec93138e127de19263a9c43c
miniedwins <miniedwins@gmail.com>
2021-09-26 10:09:04 +0800

    Update class_singleton.py

--- coding/python/example/class_singleton.py
+++ coding/python/example/class_singleton.py
@@ -4,7 +4,7 @@ class Singleton:
 
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
+            cls.__instance = super().__new__(cls, *args, **kwargs)
             return cls.__instance
         else:
             return cls.__instance
